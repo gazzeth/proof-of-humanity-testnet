b9b0a49ab2f325b0992a7e3e3da8920a6a506d1a
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-09 20:36:43 -0300

    ProofOfHumanity impl improved

--- contracts/ProofOfHumanity.sol
+++ contracts/ProofOfHumanity.sol
@@ -5,13 +5,59 @@ import { IProofOfHumanity } from "./interfaces/IProofOfHumanity.sol";
 
 contract ProofOfHumanity is IProofOfHumanity {
 
-    mapping(address => bool) registeredAddresses;
-
-    constructor() {
-        registeredAddresses[msg.sender] = true;
+    struct Register {
+        bool submitted;
+        bool registered;
+        bool superUser;
     }
 
-    function isRegistered(address _submissionAddress) override external view returns (bool) {
-        return registeredAddresses[_submissionAddress];
+    bool superUserAllowed;
+    bool selfRegistrationAllowed;
+    mapping(address => Register) registry;
+
+    constructor(bool _superUserAllowed, bool _selfRegistrationAllowed) {
+        superUserAllowed = _superUserAllowed;
+        selfRegistrationAllowed = _selfRegistrationAllowed;
+        registry[msg.sender] = Register(true, true, _superUserAllowed);
+    }
+
+    function isSuperUserAllowed() external view returns (bool) {
+        return superUserAllowed;
+    }
+
+    function isSelfVouchingAllowed() external view returns (bool) {
+        return superUserAllowed;
+    }
+
+    function isSubmitted(address _address) external view returns (bool) {
+        return registry[_address].submitted;
+    }
+
+    function isRegistered(address _address) override external view returns (bool) {
+        return registry[_address].registered;
+    }
+
+    function isSuperUser(address _address) external view returns (bool) {
+        return registry[_address].superUser;
+    }
+
+    function submit() external {
+        require(!registry[msg.sender].submitted, "Already submitted");
+        registry[msg.sender].submitted = true;
+    }
+
+    function register(address _address) external {
+        require(msg.sender != _address || selfRegistrationAllowed, "Self registration not allowed in this version");
+        if (msg.sender != _address) {
+            require(registry[msg.sender].registered, "You must be registered before registering others");
+            require(registry[msg.sender].submitted, "Address must perform submission before being registered");
+        }
+        registry[_address].registered = true;
+    }
+
+    function sudoRegister(address _address, bool _submitted, bool _registered, bool _superUser) external {
+        require(superUserAllowed, "Super user not allowed in this version");
+        require(registry[msg.sender].superUser, "You must be a super user");
+        registry[_address] = Register(_submitted, _registered, _superUser);
     }
 }
