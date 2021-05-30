72baa85455cd36b91b7403140e86a937f03caf0b
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-30 14:33:01 -0300

    Config commented to let compile successfuly

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
 require("solidity-coverage");
 
-const INFURA_PROJECT_ID = "YOUR_INFURA_PROJECT_ID";
+// const INFURA_PROJECT_ID = "YOUR_INFURA_PROJECT_ID";
 
-const ROPSTEN_PRIVATE_KEY = "YOUR_ROPSTEN_PRIVATE_KEY";
+// const ROPSTEN_PRIVATE_KEY = "YOUR_ROPSTEN_PRIVATE_KEY";
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.7.6",
   networks: {
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
-    }
+    // ropsten: {
+    //   url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+    //   accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    // }
   }
 };
