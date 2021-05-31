717d28ff160daf3f78b22b6d50b51cf2bf08674e
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-31 01:37:18 -0300

    hardhat-etherscan dependencies added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
 require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan");
 
 // const INFURA_PROJECT_ID = "YOUR_INFURA_PROJECT_ID";
 
 // const ROPSTEN_PRIVATE_KEY = "YOUR_ROPSTEN_PRIVATE_KEY";
+
+// const ETHERSCAN_API_KEY = "YOUR_ETHERSCAN_API_KEY"
 
 module.exports = {
   solidity: "0.7.6",
@@ -12,5 +15,8 @@ module.exports = {
     //   url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
     //   accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
     // }
+  },
+  etherscan: {
+    // apiKey: `${ETHERSCAN_API_KEY}`
   }
 };
